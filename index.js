8e995342ee9c87ee465b70de03490aece83920db
Henry Luan <henryluan95@gmail.com>
2022-10-24 13:26:22 -0400

    Connect with MongoDB

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.listen(() => {
-  console.log(`Listening `);
-});
+const CONNECTION_URL =
+  "mongodb+srv://memories:memories123@cluster0.uevplwh.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
